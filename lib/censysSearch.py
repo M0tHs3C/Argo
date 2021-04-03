6d5c661b046131c60fd852c6d46560192ffbefc6
M0tHs3C <32846775+M0tHs3C@users.noreply.github.com>
2021-04-03 13:56:07 +0200

    fixed a bug

--- lib/censysSearch.py
+++ lib/censysSearch.py
@@ -16,49 +16,52 @@ class censysSearch:
             with open(path + "/api/censys_api.txt", "w") as api:
                 api.write(api_censys_uid + "\n" + api_censys_scrt)
         else:
-            uid = censys_list[0]
-            secret = censys_list[1]
             try:
-                menuSelection = [['Cameras','List of cameras query and affiliated'],
-                                 ['VPNs', 'List of affected vpns']]
-                cameraSelection = [['Hikvision','Apps-webs 200 OK'],
-                             ['RSP device','WIP'],
-                             ['Viola DVR','WIP'],
-                             ['Bticino','My-Home Bticino'],
-                             ['GeoVision','Beta-testing'],
-                             ['GoAhead','GoAhead cctv'],
-                             ['Custom query','customize your search']]
-                vpnSelection = [['Fortinet FortiOS', 'vuln vpns'],
-                                ['SAMIP VoIP', 'Samip voip brute']]
-                menuBuilder.buildMenu(selectionArray=menuSelection,title="Censys search")
+                uid = censys_list[0]
+                secret = censys_list[1]
                 try:
-                    selection = int(input('[-]Choose an option: '))
-                    if selection == 1:
-                        menuBuilder.buildMenu(selectionArray=cameraSelection,title="Censys camera search")
-                        query = queryBuilder.QueryBuilder.CamQueryBuilderCensys(self)
-                    elif selection ==2:
-                        menuBuilder.buildMenu(selectionArray=vpnSelection, title="Censys vpn search")
-                        query = queryBuilder.QueryBuilder.VpnsQueryBuilderCensys(self)
-                except ValueError:
-                    print("[!!!] Wrong input value.")
-                if query != None:
-                    print('[Selected query] %s' % query)
-                else:
+                    menuSelection = [['Cameras','List of cameras query and affiliated'],
+                                    ['VPNs', 'List of affected vpns']]
+                    cameraSelection = [['Hikvision','Apps-webs 200 OK'],
+                                ['RSP device','WIP'],
+                                ['Viola DVR','WIP'],
+                                ['Bticino','My-Home Bticino'],
+                                ['GeoVision','Beta-testing'],
+                                ['GoAhead','GoAhead cctv'],
+                                ['Custom query','customize your search']]
+                    vpnSelection = [['Fortinet FortiOS', 'vuln vpns'],
+                                    ['SAMIP VoIP', 'Samip voip brute']]
+                    menuBuilder.buildMenu(selectionArray=menuSelection,title="Censys search")
+                    try:
+                        selection = int(input('[-]Choose an option: '))
+                        if selection == 1:
+                            menuBuilder.buildMenu(selectionArray=cameraSelection,title="Censys camera search")
+                            query = queryBuilder.QueryBuilder.CamQueryBuilderCensys(self)
+                        elif selection ==2:
+                            menuBuilder.buildMenu(selectionArray=vpnSelection, title="Censys vpn search")
+                            query = queryBuilder.QueryBuilder.VpnsQueryBuilderCensys(self)
+                    except ValueError:
+                        print("[!!!] Wrong input value.")
+                    if query != None:
+                        print('[Selected query] %s' % query)
+                    else:
+                        pass
+                    for record in censys.ipv4.CensysIPv4(api_id=uid, api_secret=secret).search(query):
+                        ip = record['ip']
+                        port = record['protocols']
+                        port_raw = port[0]
+                        port = re.findall(r'\d+', port_raw)
+                        with open(path + '/host/host.txt', "a") as cen:
+                            cen.write(ip + ":" + str(port[0]))
+                            cen.write("\n")
+                except KeyboardInterrupt:
+                    print("[*]Exiting...")
+                except CensysException:
                     pass
-                for record in censys.ipv4.CensysIPv4(api_id=uid, api_secret=secret).search(query):
-                    ip = record['ip']
-                    port = record['protocols']
-                    port_raw = port[0]
-                    port = re.findall(r'\d+', port_raw)
-                    with open(path + '/host/host.txt', "a") as cen:
-                        cen.write(ip + ":" + str(port[0]))
-                        cen.write("\n")
-            except KeyboardInterrupt:
-                print("[*]Exiting...")
-            except CensysException:
+                except UnboundLocalError:
+                    print("[No query passed]")
+            except:
                 pass
-            except UnboundLocalError:
-                print("[No query passed]")
     def samipGathCensys(self):
         path = os.path.abspath(os.path.dirname(sys.argv[0]))
         censys_list = open(path + "/api/censys_api.txt", "r").read().splitlines()
